dee28f464b2569c1236725948b01159d5c7d9dac
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-11-17 16:41:55 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    let capString = string.charAt(0).toUpperCase() + string.slice(1)
+    return capString
+    }
+
+  static sanitize(string) {
+    const regex = /[^-' A-Za-z0-9]/g
+    let newString = string.replace(regex, "")
+    return newString
+  }
+
+  static titleize(string) {
+    let array = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = string.split(" ")
+    
+
+    return newString.map((word, i) => {
+      return array.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ");
+  }
 }
